cd32461722fe30b9ec34cc9c9228736cdf6f2a83
Harun Ardiansyah <harunardi13@gmail.com>
2025-09-08 12:57:21 -0500

    edit 3D HTTR AVS case

--- INPUTS/OBJECTIVES3_TEST07_3DTriMG_HTTR_AVS.py
+++ INPUTS/OBJECTIVES3_TEST07_3DTriMG_HTTR_AVS.py
@@ -84,7 +84,7 @@ v = [v1_hexx, v2_hexx]
 # Noise Sources
 dTOT1 = [0] * N
 dTOT2 = [0] * N
-dTOT2[4*(J_max * I_max) + 100] = 0.05 * ABS[1][4][100]
+dTOT2[4*(J_max * I_max) + 99] = 0.05 * ABS[1][4][99]
 dTOT = [dTOT1, dTOT2]
 
 dNUFIS = [[0] * N for _ in range(group)]
--- SRC/MAIN.py
+++ SRC/MAIN.py
@@ -60,7 +60,7 @@ sys.path.append('../')
 #from INPUTS.OBJECTIVES1_TEST06_3DMG_CSTest09_VandV_new import *
 #from INPUTS.OBJECTIVES1_TEST07_3DTriMG_VVER400_VandV import *
 #from INPUTS.OBJECTIVES1_TEST08_2DTriMG_HTTR2G_VandV import *
-from INPUTS.OBJECTIVES1_TEST09_3DTriMG_HTTR import *
+#from INPUTS.OBJECTIVES1_TEST09_3DTriMG_HTTR import *
 #from INPUTS.OBJECTIVES1_TEST10_3DMG_Langenbuch import *
 
 #from INPUTS.OBJECTIVES3_TEST01_2DMG_BIBLIS_AVS import *
@@ -79,7 +79,7 @@ from INPUTS.OBJECTIVES1_TEST09_3DTriMG_HTTR import *
 #from INPUTS.OTHERS_PAPERHTTR_TEST04_2DTriMG_HTTR_FAV import *
 #from INPUTS.OTHERS_PAPERHTTR_TEST05_3DMG_CSTest09_AVS import *
 #from INPUTS.OTHERS_PAPERHTTR_TEST06_3DMG_CSTest09_FAV import *
-#from INPUTS.OTHERS_PAPERHTTR_TEST07_3DTriMG_HTTR_AVS import *
+from INPUTS.OTHERS_PAPERHTTR_TEST07_3DTriMG_HTTR_AVS import *
 #from INPUTS.OTHERS_PAPERHTTR_TEST08_3DTriMG_HTTR_FAV import *
 
 #from INPUTS.TASK3_TEST04_2DTriMG_HTTR2G_FAV import *
@@ -89,8 +89,8 @@ sys.path = original_sys_path
 
 #######################################################################################################
 #solver_type = 'forward'
-solver_type = 'adjoint'
-#solver_type = 'noise'
+#solver_type = 'adjoint'
+solver_type = 'noise'
 
 #######################################################################################################
 def main():
